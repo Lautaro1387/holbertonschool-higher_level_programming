3e400f6e12a4ce940edbdc50cc853fc4611d0b1f
Lautaro1387 <lautaro12387@gmail.com>
2022-09-06 13:02:36 -0700

    Taks 0 and 1

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = random.randint(-10, 10)
+if last_digit != 0:
+    if number > 5:
+        print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    elif number == 0:
+        print(f"Last digit of {number} is {last_digit} and is 0")
+    elif number < 6 != 0:
+        print(f"Last diigt of {number} is {last_digit} and is less than 6 and not 0")
