e6ed50c5134de2a8a10f9fabf0c4c5c2010ac936
Lautaro1387 <lautaro12387@gmail.com>
2022-09-07 08:29:34 -0700

    Taks

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i == 99:
-            print("{}{}, ".format(i, j), end="")
-        else:
-            print()
+for i in range(0, 100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02d}".format(i), end=", ")
