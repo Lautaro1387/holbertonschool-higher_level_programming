56e84ecdca8bfaa14be9aea4f37503d0c451aaf0
Lautaro1387 <lautaro12387@gmail.com>
2022-09-07 08:17:08 -0700

    Taks

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ else:
 
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
-if last_digit == 0:
+elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
-if last_digit % 10 < 6 and last_digit != 0:
+elif last_digit % 10 < 6 != 0:
     print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(0, 10):
-        print(f"{i}{j}, ", end="")
+        if i == 99:
+            print("{}{}, ".format(i, j), end="")
+        else:
+            print()
