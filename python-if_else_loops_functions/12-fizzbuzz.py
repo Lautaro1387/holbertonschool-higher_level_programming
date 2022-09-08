7eae0694aaab4318858ae887a9415fa361911846
Lautaro1387 <lautaro12387@gmail.com>
2022-09-07 19:32:06 -0700

    Fix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for test in range (1, 101):
+    for test in range(1, 101):
         if test % 3 == 0 and test % 5 == 0:
             print("FizzBuzz", end=" ")
         elif test % 3 == 0:
