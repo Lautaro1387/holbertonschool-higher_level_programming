5e11c33e371a747a2477696603273f3dd9e4c3ca
Lautaro1387 <lautaro12387@gmail.com>
2022-09-12 10:25:43 -0700

    Taks

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    my_list.copy()
+    for idx in range(len(my_list)):
+        if idx < 0:
+            return my_list[idx]
+        if idx > (len(my_list)):
+            return my_list[idx]
+        return my_list.copy()
+        
---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    for number in range(len(my_string)):
+        pop(c) and pop(C)
+        return my_string
