5a7edcf6754af5d12385fff284ba7da94adc17c1
Lautaro1387 <lautaro12387@gmail.com>
2022-09-13 06:15:41 -0700

    Taks

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    my_list.copy()
-    for idx in range(len(my_list)):
-        if idx < 0:
-            return my_list[idx]
-        if idx > (len(my_list)):
-            return my_list[idx]
-        return my_list.copy()
-        
+    new_list = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        new_list[idx] = element
+        return new_list
--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    for number in range(len(matrix)):
+        for number2 in range(len(matrix))
+            print("{:d}".format(matrix[number][number2]), end=" ")
