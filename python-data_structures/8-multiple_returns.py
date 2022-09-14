d994a7615321cb009ceb914e6ed5c63627e27322
Lautaro1387 <lautaro12387@gmail.com>
2022-09-14 10:55:21 -0700

    Taks

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     new_sen = ()
     if len(sentence) == 0:
-        new_sen == None, 0
+        new_sen == 0, "None"
     new_sen = len(sentence), sentence[0]
     return new_sen
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
     max_list = my_list[0]
-    for number in my_list:
-        if number == 0:
-            return None
-        if my_list == 0:
-            return None
-        if number > max_list:
-            max_list = number
+    for number in range(len(my_list)):
+        if my_list[number] > max_list:
+            max_list = my_list[number]
             return max_list
