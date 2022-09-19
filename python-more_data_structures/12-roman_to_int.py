a0139c9720d544bb572fb8593680e2aba6c507c7
Lautaro1387 <lautaro12387@gmail.com>
2022-09-18 17:43:25 -0700

    Taks

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+	if type(roman_string) != str or roman_string is None:
+		return 0
+	roman = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+	x = 0
+	aux = []
+	for i in roman_string:
+		aux.append(roman[i])
+	x2 = [0]
+	x2.extend(aux)
+	aux.append(0)
+	for j, w in zip(reversed(x2), reversed(aux)):
+		if j != 0:
+			if j >= w:
+				x += j
+			else:
+				x -= j
+	return x
