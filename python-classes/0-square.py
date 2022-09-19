9baa6a200b6adc70afdede9bd17d56cd7807c2d9
Lautaro1387 <lautaro12387@gmail.com>
2022-09-19 08:11:06 -0700

    Documented modules

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+"""My first Square"""
 class Square:
+    """class name: Square"""
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""My Second Square"""
 class Square:
+    """def name: __init__"""
     def __init__(self, size):
+        """__size = size"""
         self.__size = size
         
