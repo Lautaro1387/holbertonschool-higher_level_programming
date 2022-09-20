7caec9db5e4106d59e4fa6d85d316450c2a4d65d
Lautaro1387 <lautaro12387@gmail.com>
2022-09-20 07:45:04 -0700

    Taks 3 and 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,12 +5,15 @@
 class Square:
     """class name: Square"""
     def __init__(self, size=0):
-        """def name: __init__"""
-        if int(size) < 0:
+        """class name: __init__"""
+        if size < 0:
             raise ValueError("size must be >= 0")
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        self.__size = size
+    def size(self):
+        """def name: size"""
+    def size(self, value):
+        """def name: size"""
     def area(self):
         """def name: area"""
         return self.__size * self.__size
