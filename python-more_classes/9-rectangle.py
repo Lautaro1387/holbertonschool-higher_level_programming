4afd949f6d1dc294996c63777fcc189b5e400045
Lautaro1387 <lautaro12387@gmail.com>
2022-09-22 07:38:22 -0700

    Fix taks 6 and 9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -9,6 +9,14 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """def __init__"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif int(width) < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif int(height) < 0:
+            raise ValueError("height must be >= 0")
         self.height = height
         self.width = width
         self.height = height
@@ -28,7 +36,7 @@ class Rectangle:
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("widht must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
