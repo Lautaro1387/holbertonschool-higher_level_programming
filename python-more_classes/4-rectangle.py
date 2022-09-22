abf520c1f9c63e5bba628e48495cac125d7af437
Lautaro1387 <lautaro12387@gmail.com>
2022-09-21 19:21:11 -0700

    Fix

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -55,4 +55,5 @@ class Rectangle:
         return string
 
     def __repr__(self):
-        return (type(self).__name__ + "(" + str(self.__width) + ", " + str(self.__height) + ")")
+        return (type(self).__name__ + "(" + str(self.__width) + 
+                ", " + str(self.__height) + ")")
