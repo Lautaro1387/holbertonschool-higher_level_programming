ce7985896a5eff84f55cab62e96869796d2d93cb
Lautaro1387 <lautaro12387@gmail.com>
2022-09-23 06:44:12 -0700

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""add_integer"""
+
 def add_integer(a, b=98):
-    if a is not int or float:
+    """add_integer"""
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if b is not int or float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
     return a + b
---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    if matrix not is int or float:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if div not is int or float:
+        raise TypeError("div must be a number")
+    if div not is 0
