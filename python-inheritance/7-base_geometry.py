ea99b5916aa9be831e2898a24518e5c334e38f5f
Lautaro1387 <lautaro12387@gmail.com>
2022-09-26 18:50:32 -0700

    Task 0, 1, 2, 3, 4, 5, 6, 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class name: BaseGeometry"""
+    def area(self):
+        """Geometry base area"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """That validates value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0 or value == 0:
+            raise ValueError("{} must be greater than 0".format(name))
