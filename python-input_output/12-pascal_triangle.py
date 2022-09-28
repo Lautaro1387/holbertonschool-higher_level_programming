341b5203a98f664fa0ded0767f429c12b3ec3163
Lautaro1387 <lautaro12387@gmail.com>
2022-09-28 16:26:08 -0700

    Function Pascal triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -10,6 +10,6 @@ def pascal_triangle(n):
     for i in range(n):
         lis.append([1] * (i + 1))
     for i in range(1, len(lis)):
-       for j in range(1, len(lis[i]) - 1):
+        for j in range(1, len(lis[i]) - 1):
             lis[i][j] = lis[i - 1][j - 1] + lis[i - 1][j]
     return lis
