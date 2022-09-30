dafe3c51ce4ddef08c4983a62cb37a629c5d07cd
Lautaro1387 <lautaro12387@gmail.com>
2022-09-30 09:32:52 -0700

    Fix the documentation and add method area and validation and instantiation in class Rectangle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,7 +5,9 @@
 class Base:
     """class name: Base"""
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """ class constructor"""
         if id is not None:
             self.id = id
         else:
