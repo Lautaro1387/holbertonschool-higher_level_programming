cc1132e52d92343fe42cae925301517a04941614
Lautaro1387 <lautaro12387@gmail.com>
2022-10-04 11:56:57 -0700

    Fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -85,7 +85,8 @@ class Rectangle(Base):
     def __str__(self):
         """string representation of a rectangle"""
 
-        return f'[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}'
+        return f'[Rectangle] ({self.id}) {self.__x}/{self.__y} -
+                {self.__width}/{self.__height}'
 
     def update(self, *args, **kwargs):
         """Attributes of an instance"""
