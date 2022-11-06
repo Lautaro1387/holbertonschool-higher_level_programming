17954c0b965021fe38cede1339fc0934a9d86427
lautaroilla <lautaro12387@gmail.com>
2022-11-06 19:59:37 -0300

    Task 15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Lists the number of records
-SELECT score, COUNT(score) as number FROM second_table ORDER BY score, number desc GROUP BY score, number;
+SELECT score, COUNT(score) as number FROM second_table ORDER BY score, number desc GROUP BY number;
