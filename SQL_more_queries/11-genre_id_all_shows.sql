fb9e44aa957acd9216b6f96a71059b33ce37f748
lautaroilla <lautaro12387@gmail.com>
2022-11-06 23:37:22 -0300

    Task 11 and 12

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
 -- Lists all contained in hbtn_0d_tvshows.
-SELECT tv_show.title, tv_show_genres.genre_id
-FROM tv_show LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_show.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
