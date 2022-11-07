a76cdce07926c9f7411c22da7192f8d4317921fb
lautaroilla <lautaro12387@gmail.com>
2022-11-06 23:22:27 -0300

    Task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- Lists all shows contained in hbtn_0d_tvshows
 SELECT tv_show.title, tv_show_genrs.genre_id
-FROM tv_show JOIN tv_show_genres ON tv_show.id = tv_show_genres.id
+FROM tv_show JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
 ORDER BY tv_show.title, tv_show_genres.genr_id ASC;
