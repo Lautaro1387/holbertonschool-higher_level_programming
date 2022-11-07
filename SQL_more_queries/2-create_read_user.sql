2cbfe641bc58939a1b04bc06bcf6785147cf0782
lautaroilla <lautaro12387@gmail.com>
2022-11-06 21:17:12 -0300

    Task 2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+DROP USER 
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'@'localhost';
+GRANT SELECT DATABASE IF NOT EXISTS 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
