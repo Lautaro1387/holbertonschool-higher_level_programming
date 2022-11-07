11fcb61296183b3abb5ba61e7aa018a8bbbdc93c
lautaroilla <lautaro12387@gmail.com>
2022-11-06 22:06:25 -0300

    Task 4 and 5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Creates the table unique_id
-CREATE TABLE IF NOT EXISTS `unique_id` (id INT default value 1 and unique, name VARCHAR(256) NOT NULL):
+CREATE TABLE IF NOT EXISTS unique_id (id INT unique default '1' , name VARCHAR(256) NOT NULL):
