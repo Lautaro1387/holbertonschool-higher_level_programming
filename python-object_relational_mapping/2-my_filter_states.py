b5fe9a1ee05d88e584f2e50e98b9e149233b137c
Lautaro1387 <lautaro12387@gmail.com>
2022-11-09 20:30:37 +0000

    Task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    takes in an argument and displays all values in
+    the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+
+from MySQLdb import connect
+import sys
+
+if __name__ == "__main__":
+    _user = sys.argv[1]
+    _pass = sys.argv[2]
+    _db = sys.argv[3]
+    _st = sys.argv[4]
+
+    db = connect(host="localhost", port=3006, user=_user, passwd=_pass, db=_db, st=_st)
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states ORDER BY states.id asc")
+    rows = curs.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
