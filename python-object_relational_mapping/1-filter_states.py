275f0b178ae8f6253ce41f6c9286d66c163b5a93
Lautaro1387 <lautaro12387@gmail.com>
2022-11-09 19:03:33 +0000

    Task 0 and 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""Lists all states from the databases hbtn_0e_0_usa"""
+"""lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
 
 
-from MySQLdb import connect
+import MySQLdb
 import sys
 
-
-if __name__ == "__main__":
-    """Access (database)"""
+if __name__ = "__main__":
     _user = sys.argv[1]
     _passwd = sys.argv[2]
     _db = sys.argv[3]
 
-    db = connect(host="localhost", port=3306, user=_user, passwd=_passwd, db=_db)
+    db = MySQLdb.connect(host="localhost", port=3006, user=_user, passwd=_passwd, db=_db)
     curs = db.cursor()
     curs.execute("SELECT * FROM states ORDER BY states.id asc")
     rows = curs.fetchall()
     for row in rows:
+        row[1] == 'N'
         print(row)
