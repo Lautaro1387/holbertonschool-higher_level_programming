09090bd0d561d2bcc999db9ed3c41e80c93c98cc
Lautaro1387 <lautaro12387@gmail.com>
2022-11-09 18:11:26 +0000

    Task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+"""Lists all states from the databases hbtn_0e_0_usa"""
+
+from MySQLdb import connect
+import sys
 
 
-import MySQLdb
+if __name__ == "__main__":
+    _user = sys.argv[1]
+    _pass = sys.argv[2]
+    _db = sys.argv[3]
 
-
+    db = connect(host="localhost", port=3306, user=_user, passwd=_pass, db=_db)
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states ORDER BY states.id")
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    db.close()
