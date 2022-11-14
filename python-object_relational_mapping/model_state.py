bad1ae3370bd0c6cf50fef00616906c6290b3c83
Lautaro1387 <lautaro12387@gmail.com>
2022-11-13 16:34:17 -0800

    Fix task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-file that contains the class definition of a 
+file that contains the class definition of a
 State and an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
