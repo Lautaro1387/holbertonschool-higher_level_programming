96e37a2731ab6b8cd58238d34f527cfeea93bb28
Lautaro1308 <lautaro12387@gmail.com>
2022-12-07 18:56:53 -0300

    Fix

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const argv = require('process').argv
+const argv = require('process').argv;
 if (argv.length < 3) {
-    console.log("No argument")
+  console.log('No argument');
 } else {
-    console.log(argv[2])
+  console.log(argv[2]);
 }
