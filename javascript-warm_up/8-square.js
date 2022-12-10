81058de9e71c34b449ead998e6673a448020e467
Lautaro1308 <lautaro12387@gmail.com>
2022-12-10 02:23:38 -0300

    semistandard fix

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,10 +4,10 @@ const argv = require('process').argv;
 if (isNaN(argv[2])) {
   console.log('Missing size');
 } else {
-    for (let i = 0; i < argv[2]; i++) {
-      for (let j = 0; j < argv[2]; j++) {
-        process.stdout.write("X");
-      }
-      console.log();
+  for (let i = 0; i < argv[2]; i++) {
+    for (let j = 0; j < argv[2]; j++) {
+      process.stdout.write('X');
     }
+    console.log();
+  }
 }
--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 const argv = require('process').argv;
 
-function add(a, b) {
-    return (a + b)
-}
 if (isNaN(argv[2] && argv[3])) {
-    console.log('NaN');
+  console.log('NaN');
 } else {
-    console.log(+argv[2] + +argv[3])
+  console.log(+argv[2] + +argv[3]);
 }
 /*
 Alternative
