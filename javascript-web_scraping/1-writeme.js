8f2fe9907028a9a6734a3b6937bdc516bd22d309
root <root@H45.hbtn.io>
2022-12-14 15:15:03 -0300

    Task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argv = require('process').argv;
+
+const fs = require('fs');
+fs.writeFile(argv[2], argv[3], function (error, response) {
+    if (error) {
+        throw error;
+    }
+    console.log(response)
+})
---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const GET = require('process').GET
+
+console.log('code: ')
