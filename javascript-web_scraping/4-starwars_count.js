477a95b1123ed7a6345788d17e62f38ace83f57d
Lautaro1308 <lautaro12387@gmail.com>
2022-12-15 18:53:54 -0300

    Task 4 and 5

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,5 +6,14 @@ request(argv[2], function (error, response, body) {
   if (error) {
     throw error;
   }
-  console.log(JSON.parse(body));
+  let count = 0;
+  const all = JSON.parse(body).results;
+  for (let i = 0; i < all.length; i++) {
+    for (let j = 0; j < all[i].characters.length; j++) {
+      if (all[i].characters[j].includes('/18/')) {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
 });
