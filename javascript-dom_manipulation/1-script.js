ae15aef6552a3bd86da7480ffe36b766edd91623
Lautaro1308 <lautaro12387@gmail.com>
2022-12-14 22:44:30 -0300

    Task 0 and 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+Write a JavaScript script that updates the text color
+of the `header` element to red (`#FF0000`)
+when the user clicks on the tag with id `red_header`
+*/
+document.querySelector('header').style.color = '#FF0000';
