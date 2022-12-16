3b7e657e6984ed92ace29d80cc3cf925a0a15c96
root <root@H45.hbtn.io>
2022-12-16 11:16:48 -0300

    Task 0 and 1

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -4,4 +4,5 @@ Write a JavaScript script that updates the text color
 of the `header` element to red (`#FF0000`)
 when the user clicks on the tag with id `red_header`
 */
-document.querySelector('header').style.color = '#FF0000';
+const colors = document.querySelector('header').style.color = '#FF0000';
+const clicked = document.addEventListener("click", colors);
---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/*
+Write a JavaScript script that adds the class red to the header element
+when the user clicks on the tag with id red_header
+*/
+document.querySelector('header')
