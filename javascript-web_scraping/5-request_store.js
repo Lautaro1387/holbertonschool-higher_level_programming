26355a305673c09f87982378ce33f0436dfe8830
Lautaro1308 <lautaro12387@gmail.com>
2022-12-15 21:05:47 -0300

    Task 6

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const argv = require('process').argv;
 const request = require('request');
-const fs = require ('fs');
+const fs = require('fs');
 
-request(argv[2], function(error, response, body) {
+request(argv[2], function (error, response, body) {
+  if (error) {
+    throw error;
+  }
+  fs.writeFile(argv[3], body, 'utf-8', function (error) {
     if (error) {
-        throw error;
+      throw error;
     }
-    fs.writeFile(argv[3], '/loripsum', 'utf-8', function (error) {
-        if (error) {
-            throw error;
-        }
-    });
+  });
 });
---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const argv = require('process').argv;
+const request = require('request');
+
+request(argv[2], function (error, response, body) {
+  if (error) {
+    throw error;
+  }
+  const task = {};
+  const all = JSON.parse(body);
+  for (let i = 0; i < all.length; i++) {
+    if (all[i].completed === true) {
+      if (all[i].userId in task) {
+        task[all[i].userId] += 1;
+      } else {
+        task[all[i].userId] = 1;
+      }
+    }
+  }
+  console.log(task);
+});
