355faa9775be77f99d5f98a0c7a2757006edeaa5
Lautaro1308 <lautaro12387@gmail.com>
2022-12-18 23:30:27 -0300

    Fix

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -4,5 +4,8 @@ Write a JavaScript script that updates the text color
 of the `header` element to red (`#FF0000`)
 when the user clicks on the tag with id `red_header`
 */
-const colors = document.querySelector('header').style.color = '#FF0000';
-const clicked = document.addEventListener("click", colors);
+const head = document.querySelector('header');
+const clicked = document.querySelector('#red_header');
+clicked.addEventListener("click", ()=>{
+    head.style.color = '#FF0000';
+  })
